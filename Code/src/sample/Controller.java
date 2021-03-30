2ea44e6f283ca6392e09670760f553beb6f572f5
kkviks <kkviks@gmail.com>
2021-03-30 18:30:04 +0530

    Add Login Controller

--- Code/src/sample/Controller.java
+++ Code/src/sample/Controller.java
@@ -1,4 +1,57 @@
 package sample;
 
-public class Controller {
+
+import javafx.application.Platform;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private TextField txtEid;
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private Button btn_clear, btn_signIn, btn_signUp, btn_close;
+
+    @FXML
+    private Label lbl_forgotPassword, lblErrors, lbl_msg;
+
+    @FXML ImageView img_close;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        lbl_msg.setText("");
+    }
+
+    public void clear(){
+        txtEid.setText("");
+        txtPassword.setText("");
+    }
+
+    public void referAdmin(){
+        lbl_msg.setText("Contact Admin!");
+    }
+
+    public void clearReferAdmin(){
+        lbl_msg.setText("");
+    }
+
+    public void exit(){
+        Platform.exit();
+        System.exit(0);
+    }
 }
--- Code/src/sample/Main.java
+++ Code/src/sample/Main.java
@@ -1,19 +1,52 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
+    private double xOffset = 0;
+    private double yOffset = 0;
+    private Parent root;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception{
+        try
+        {
+
+            root = FXMLLoader.load(getClass().getResource("login.fxml"));
+
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setMaximized(false);
+
+            root.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    xOffset = event.getSceneX();
+                    yOffset = event.getSceneY();
+                }
+            });
+
+            root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    stage.setX(event.getScreenX() - xOffset);
+                    stage.setY(event.getScreenY() - yOffset);
+                }
+            });
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
 
