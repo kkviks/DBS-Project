4fe025b67aee60cf4a8ea25b0aa740e92d111100
kkviks <kkviks@gmail.com>
2021-04-01 17:57:20 +0530

    Context Switch in Front Desk

--- Code/src/sample/ReceptionController.java
+++ Code/src/sample/ReceptionController.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -24,19 +25,30 @@ public class ReceptionController implements Initializable {
     private Button btnOrders;
 
     @FXML
+    private Button btnStaff;
+
+    @FXML
+    private Button btnCheckout;
+
+    @FXML
+    private Button btnNewBooking;
+
+    @FXML
     private Button btnCustomers;
 
     @FXML
-    private Button btnMenus;
-
-    @FXML
-    private Button btnPackages;
-
-    @FXML
-    private Button btnSettings;
-
-    @FXML
     private Button btnSignout;
+
+    //panels
+
+    @FXML
+    private Pane pnlOverview;
+
+    @FXML
+    private Pane pnlNewBooking;
+
+    @FXML
+    private Pane pnlStaff;
 
     @FXML
     private Pane pnlCustomer;
@@ -45,10 +57,8 @@ public class ReceptionController implements Initializable {
     private Pane pnlOrders;
 
     @FXML
-    private Pane pnlOverview;
+    private Pane pnlCheckout;
 
-    @FXML
-    private Pane pnlMenus;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -58,7 +68,6 @@ public class ReceptionController implements Initializable {
 
                 final int j = i;
                 nodes[i] = FXMLLoader.load(getClass().getResource("Item.fxml"));
-
                 //give the items some effect
 
                 nodes[i].setOnMouseEntered(event -> {
@@ -72,27 +81,86 @@ public class ReceptionController implements Initializable {
                 e.printStackTrace();
             }
         }
+        hideAll();
+        showOnly("Overview");
+    }
 
+    private void hideAll() {
+        pnlOverview.setVisible(false);
+        pnlNewBooking.setVisible(false);
+        pnlStaff.setVisible(false);
+        pnlCustomer.setVisible(false);
+        pnlOrders.setVisible(false);
+        ;
+        pnlCheckout.setVisible(false);
+    }
+
+    private void showOnly(String panel) {
+        switch (panel) {
+            case "Overview":
+                pnlOverview.toFront();
+                pnlOverview.setVisible(true);
+                pnlOverview.setStyle("-fx-background-color : #02030A");
+                break;
+            case "New Booking":
+                pnlNewBooking.toFront();
+                pnlNewBooking.setVisible(true);
+                pnlNewBooking.setStyle("-fx-background-color : #02030A");
+                break;
+            case "Staff":
+                pnlStaff.toFront();
+                pnlStaff.setVisible(true);
+                pnlStaff.setStyle("-fx-background-color : #02030A");
+                break;
+            case "Customers":
+                pnlCustomer.toFront();
+                pnlCustomer.setVisible(true);
+                pnlCustomer.setStyle("-fx-background-color : #02030A");
+                break;
+            case "Orders":
+                pnlOrders.toFront();
+                pnlOrders.setVisible(true);
+                pnlOrders.setStyle("-fx-background-color : #02030A");
+                break;
+            case "Checkout":
+                pnlCheckout.toFront();
+                pnlCheckout.setVisible(true);
+                pnlCheckout.setStyle("-fx-background-color : #02030A");
+                break;
+            default: // Do nothing
+        }
     }
 
 
     public void handleClicks(ActionEvent actionEvent) {
-        if (actionEvent.getSource() == btnCustomers) {
-            pnlCustomer.setStyle("-fx-background-color : #1620A1");
-            pnlCustomer.toFront();
-        }
-        if (actionEvent.getSource() == btnMenus) {
-            pnlMenus.setStyle("-fx-background-color : #53639F");
-            pnlMenus.toFront();
-        }
+
         if (actionEvent.getSource() == btnOverview) {
-            pnlOverview.setStyle("-fx-background-color : #02030A");
-            pnlOverview.toFront();
+            showOnly("Overview");
         }
-        if(actionEvent.getSource()==btnOrders)
-        {
-            pnlOrders.setStyle("-fx-background-color : #464F67");
-            pnlOrders.toFront();
+
+        if (actionEvent.getSource() == btnNewBooking) {
+            showOnly("New Booking");
+        }
+
+        if (actionEvent.getSource() == btnStaff) {
+            showOnly("Staff");
+        }
+
+        if (actionEvent.getSource() == btnCustomers) {
+            showOnly("Customers");
+        }
+
+        if (actionEvent.getSource() == btnOrders) {
+            showOnly("Orders");
+        }
+
+        if (actionEvent.getSource() == btnCheckout) {
+            showOnly("Checkout");
+        }
+
+        if (actionEvent.getSource() == btnSignout) {
+            Platform.exit();
+            System.exit(0);
         }
     }
 }
